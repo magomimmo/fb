adaeed24a7b91b0545680696fef67942028bc454
magomimmo <mimmo.cosenza@gmail.com>
2014-01-12 15:44:16 +0100

    remove comments from cljs-start lein-template

--- test/cljs/com/sinapsi/fb_test.cljs
+++ test/cljs/com/sinapsi/fb_test.cljs
@@ -1,5 +1,3 @@
-;;; This namespace is used for testing purpose. It use the
-;;; clojurescript.test lib.
 (ns com.sinapsi.fb-test
   (:require-macros [cemerick.cljs.test :as m :refer (deftest testing are)])
   (:require [cemerick.cljs.test :as t]
