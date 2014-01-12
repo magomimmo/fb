5d72804b2b41414e5b2225a4bfaa81de5b4c402c
magomimmo <mimmo.cosenza@gmail.com>
2014-01-12 17:43:56 +0100

    rename build

--- project.clj
+++ project.clj
@@ -9,13 +9,16 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2127"]]
   :plugins [[lein-cljsbuild "1.0.1"]]
+  :hooks [leiningen.cljsbuild]
   :cljsbuild
   {:crossovers [com.sinapsi.fb.controllers.pid
                 com.sinapsi.fb.controllers.advanced-pid
                 com.sinapsi.fb.protocols]
-   :builds {:useless
+   :builds {:fb
             {:source-paths ["src/cljs"]
              :compiler
-             {:output-to "dev-resources/public/js/useless.js"
+             {:output-to "dev-resources/public/js/fb.js"
+              :output-dir "dev-resources/public/js"
+              :source-map "dev-resources/public/js/fb.js.map"
               :optimizations :none
               :pretty-print false}}}})
