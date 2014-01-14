4dd53123637c15ac76a369a7e1456f4e2ea887d9
magomimmo <mimmo.cosenza@gmail.com>
2014-01-14 02:09:45 +0100

    add relay to crossvers

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :min-lein-version "2.3.4"
-  :source-paths ["src/clj" "src/cljs"]
+  :source-paths ["src/clj" "test/cljs"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]]
   :plugins [[lein-cljsbuild "1.0.1"]]
@@ -14,12 +14,12 @@
   {:crossovers [com.sinapsi.fb.controllers.pid
                 com.sinapsi.fb.controllers.advanced-pid
                 com.sinapsi.fb.controllers.deadband
+                com.sinapsi.fb.controllers.relay
                 com.sinapsi.fb.protocols]
+   :crossover-path "target/crossovers/cljs"
    :builds {:fb
-            {:source-paths ["src/cljs"]
+            {:source-paths ["target/crossovers/cljs"]
              :compiler
              {:output-to "dev-resources/public/js/fb.js"
-              :output-dir "dev-resources/public/js"
-              :source-map "dev-resources/public/js/fb.js.map"
-              :optimizations :none
+              :optimizations :advanced
               :pretty-print false}}}})
